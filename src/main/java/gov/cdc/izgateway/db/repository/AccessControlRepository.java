7c9dcf84f2a7d43449f5356ffd3c8e54bec84204
Keith W. Boone <kboone@ainq.com>
2024-08-05 17:11:28 -0400

    Updates to fix blacklist add/delete operations.

--- src/main/java/gov/cdc/izgateway/db/repository/AccessControlRepository.java
+++ src/main/java/gov/cdc/izgateway/db/repository/AccessControlRepository.java
@@ -4,8 +4,10 @@ package gov.cdc.izgateway.db.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import gov.cdc.izgateway.db.model.AccessControl;
 import gov.cdc.izgateway.db.model.AccessControlId;
@@ -43,5 +45,45 @@ public interface AccessControlRepository extends JpaRepository<AccessControl, Ac
 	@Override
 	default List<AccessControl> findAll() {
 		return findAllByDestTypeId(SystemUtils.getDestType());
+	}
+	
+
+	/**
+	 * Add a new access control entry.
+	 * This is ONLY called to add a blacklisted user 
+	 * @param category	The category
+	 * @param name		The name
+	 * @param member	The member
+	 * @param allow		The flag settings.
+	 */
+    @Modifying
+    @Query(value = "insert into accesscontrol(category, name, member, allow) values(?1, ?2, ?3, ?4)", nativeQuery = true)
+    void insertAccessControl(String category, String name, String member, int allow);
+    
+    /**
+     * Update a user.
+     * Presently, this is only used by IZ Gateway to create a new blacklist entry, which should always
+     * be an insert operation.  It's technically possible that two systems could be trying to create
+     * the same entry at the same time, perhaps even in two different environments. Because it's a
+     * blacklist entry, it applies to ALL environments.  A user blacklisted by one IZGW environment
+     * shouldn't be allowed into any other environments.
+     * 
+     * This will need rework to support adding users to groups generally, and so will the 
+     * delete capabilities.
+     * 
+     * @param accessControl the entry for the user to blacklist
+     * @return The stored access control entry
+     */
+    @SuppressWarnings("unchecked")
+	@Transactional
+	@Override
+	default AccessControl saveAndFlush(AccessControl accessControl) {
+		insertAccessControl(
+			accessControl.getCategory(),
+			accessControl.getName(),
+			accessControl.getMember(),
+			accessControl.isAllowed() ? 0x7F : 0x7E
+		);
+		return accessControl;
 	}
 }
