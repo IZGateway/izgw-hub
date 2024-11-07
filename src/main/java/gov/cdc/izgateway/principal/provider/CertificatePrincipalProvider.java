65993aafc69e4035560770532a72d62124a31e65
Paul Cahill <Paul.Cahill@pointclickcare.com>
2024-11-07 14:59:18 -0500

    Refactor Principal Provider interfaces to  be in core.

--- src/main/java/gov/cdc/izgateway/principal/provider/CertificatePrincipalProvider.java
+++
@@ -1,9 +0,0 @@
-// CertificatePrincipalProvider.java
-package gov.cdc.izgateway.principal.provider;
-
-import gov.cdc.izgateway.security.IzgPrincipal;
-import jakarta.servlet.http.HttpServletRequest;
-
-public interface CertificatePrincipalProvider {
-    IzgPrincipal createPrincipalFromCertificate(HttpServletRequest request);
-}
