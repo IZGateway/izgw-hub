2e52ce20902830601d24d879ffdf1f4dde82425a
Paul Cahill <pcahill@ainq.com>
2025-07-01 07:12:51 -0400

    Changes to refactor LogController with core.

--- src/main/java/gov/cdc/izgateway/LogController.java
+++ src/main/java/gov/cdc/izgateway/LogController.java
@@ -58,99 +58,48 @@ import jakarta.servlet.http.HttpServletResponse;
 // applies to the SOAP Stack.  Once we apply it to the full HTTP stack, we will have to provide
 // SECURE mechanism for clearing the blacklisted state of the testing user.  It cannot be said
 // to have been applied to the full stack until this loophole is resolved.
-@RolesAllowed({ Roles.ADMIN, Roles.OPERATIONS })
+@RolesAllowed({ Roles.ADMIN, Roles.OPERATIONS, Roles.BLACKLIST })
 @RequestMapping({"/rest"})
 @Lazy(false)
-public class LogController implements InitializingBean {
-	@Configuration(proxyBeanMethods = false) 
-	public static class LogControllerConfig {
-	    @Bean
-	    public ObjectMapper getObjectMapper() {
-	        ObjectMapper mapper = new ObjectMapper();
-	        SimpleModule simpleModule = new SimpleModule();
-	        simpleModule.addSerializer(ILoggingEvent.class, new LogstashMessageSerializer());
-	        mapper.registerModule(simpleModule);
-	        return mapper;
-		}
-	}
-	
-	private MemoryAppender logData = null;
-	private final IDestinationService destinationService;
-	private final EndpointStatusService endpointStatusService;
-	private final IAccessControlService accessControlService;
+public class LogController extends LogControllerBase {
 
-	@Autowired
-	public LogController(AccessControlRegistry registry, IDestinationService destinationService, EndpointStatusService endpointStatusService, IAccessControlService accessControlService) {
-		registry.register(this);
-		this.destinationService = destinationService;
-		this.endpointStatusService = endpointStatusService;
-		this.accessControlService = accessControlService;
-	}
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		logData = MemoryAppender.getInstance("memory");
-	}
+    private MemoryAppender logData = null;
+    private final IDestinationService destinationService;
+    private final EndpointStatusService endpointStatusService;
+    private final IAccessControlService accessControlService;
 
-	// TODO: Presently, blacklisted users are allowed to access the logs request, b/c blacklisting only
-	// applies to the SOAP Stack.  Once we apply it to the full HTTP stack, we will have to provide
-	// SECURE mechanism to clearing the state.
-	@Operation(summary = "Get the most recent log records",
-			description = "Search for the log records matching the search parameter or all records if there is no search value")
-	@ApiResponse(responseCode = "200", description = "Success", 
-    	content = { 
-    		@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = LogEvent.class))) 
-    	}
-	)
-	@GetMapping("/logs")
-	public List<LogEvent> getLogs(
-			@Parameter(description = "The search string", required = false)
-			@RequestParam(required = false) String search, 
-			HttpServletResponse resp) {
+    @Autowired
+    public LogController(AccessControlRegistry registry, IDestinationService destinationService, EndpointStatusService endpointStatusService, IAccessControlService accessControlService) {
+        super(registry);
+        this.destinationService = destinationService;
+        this.endpointStatusService = endpointStatusService;
+        this.accessControlService = accessControlService;
+    }
 
-		List<ILoggingEvent> events = null;
-		if (logData == null) {
-			events = Collections.emptyList();
-		} else if (StringUtils.isBlank(search)) {
-			events = logData.getLoggedEvents();
-		} else {
-			events = logData.search(search);
-		}
-		
-		return new ListConverter<>(events, LogEvent::new);
-	}
+    @Override
+    public void deleteLogs(HttpServletRequest servletReq,
+                           @Parameter(required = false, description="If true, reset the specified endpoint, clearing maintenance")
+                           @RequestParam(required = false) String clear) throws SecurityFault {
+        super.deleteLogs(servletReq, clear);
 
-	@Operation(summary = "Clear log records")
-	@ApiResponse(responseCode = "204", description = "Reset the logs", content = @Content)
-	@DeleteMapping("/logs")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	@RolesAllowed({ Roles.ADMIN, Roles.OPERATIONS, Roles.BLACKLIST })
-	public void deleteLogs(HttpServletRequest servletReq,
-			@Parameter(required = false, description="If true, reset the specified endpoint, clearing maintenance")
-			@RequestParam(required = false) String clear) throws SecurityFault {
-		if (!RequestContext.getRoles().contains(Roles.ADMIN) && !RequestContext.getRoles().contains(Roles.OPERATIONS)) {
-			throw SecurityFault
-				.generalSecurity("Delete Log Attempt By Role", 
-						RequestContext.getRoles().toString(), null);
-		}
-		if (logData != null) {
-			logData.reset();
-		}
-		if (clear != null) {
-			// Clear any errors from the specified destination.
-			// This allows testing to reset both logs and a destination before a new test
-			IEndpointStatus status = endpointStatusService.findById(clear);
-			if (status != null) {
-				status.setStatus(IEndpointStatus.CONNECTED);
-				clearMaintenance(clear);
-			}
-			accessControlService.removeUserFromBlacklist(RequestContext.getSourceInfo().getCommonName());
-		}
-	}
-	
-	private void clearMaintenance(String destId) {
-		IDestination dest = destinationService.findByDestId(destId);
-		if (dest != null) {
-			destinationService.clearMaintenance(dest);
-		}
-	}
+        if (clear != null) {
+            // Clear any errors from the specified destination.
+            // This allows testing to reset both logs and a destination before a new test
+            IEndpointStatus status = endpointStatusService.findById(clear);
+            if (status != null) {
+                status.setStatus(IEndpointStatus.CONNECTED);
+                clearMaintenance(clear);
+            }
+            accessControlService.removeUserFromBlacklist(RequestContext.getSourceInfo().getCommonName());
+        }
+
+    }
+
+    private void clearMaintenance(String destId) {
+        IDestination dest = destinationService.findByDestId(destId);
+        if (dest != null) {
+            destinationService.clearMaintenance(dest);
+        }
+    }
+
 }
